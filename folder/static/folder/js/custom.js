18535266d1986dda6492209138f96b754b302d4d
Andrew Burdyug <buran83@gmail.com>
2015-04-03 13:22:43 +0300

    Refactoring: merge JS functions send_signup_data and send_credentials into send_data

--- folder/static/folder/js/custom.js
+++ folder/static/folder/js/custom.js
@@ -4,56 +4,43 @@ var check_http_method = function(method){
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
-var send_credentials = function(auth_data){
+var service_ok_urls = {
+    'login': '/folder/home/',
+    'signup': '/folder/login/'
+}
+
+var format_error_msg = function(data){
+    $.each(key, value)
+}
+
+var send_data = function(service_type, data){
     $.ajax({
         beforeSend: function(xhr, settings){
             if (!check_http_method(settings.type) && !this.crossDomain) {
                 xhr.setRequestHeader("X-CSRFToken", csrftoken);
             }
         },
-        url: "/folder/login/",
+        url: "/folder/" + service_type + "/",
         method: "POST",
-        data: auth_data
-    }).done(function(data){
-        if (data.status == 'OK'){
-            home_url = window.location.origin + '/folder/home/';
+        data: data
+    }).done(function(backend_response){
+        if (backend_response.status == 'OK'){
+            home_url = window.location.origin + service_ok_urls[service_type];
             window.location.replace(home_url);
         } else {
-            $('#fail_login_alert').fadeIn(800);
-            $('#fail_login_alert').fadeOut(2000);
+            $('#fail_reason').html(backend_response.info);
+            $('#fail_' + service_type + '_alert').fadeIn(800).delay(1500).fadeOut(2000);
         }
     });
 }
-
-var send_signup_data = function(signup_data){
-    $.ajax({
-        beforeSend: function(xhr, settings){
-            if (!check_http_method(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        },
-        url: "/folder/signup/",
-        method: "POST",
-        data: signup_data
-    }).done(function(data){
-        if (data.status == 'OK'){
-            home_url = window.location.origin + '/folder/login/';
-            window.location.replace(home_url);
-        } else {
-            $('#fail_reason').html(data.info);
-            $('#fail_signup_alert').fadeIn(800).delay(1500).fadeOut(2000);
-        }
-    });
-}
-
 
 $(function() {
     csrftoken = $("input[name='csrfmiddlewaretoken']").val();
 
     $('#login_btn').click(function(event){
         event.preventDefault();
-        send_credentials({'username': $('#username').val(),
-                          'password': $('#password').val()});
+        send_data('login', {'username': $('#username').val(),
+                            'password': $('#password').val()});
     });
 
     $('#redirect_to_signup_btn').click(function(event){
@@ -64,10 +51,10 @@ $(function() {
 
     $('#signup_btn').click(function(event){
         event.preventDefault();
-        send_signup_data({'username': $('#username').val(),
-                          'password1': $('#password1').val(),
-                          'password2': $('#password2').val(),
-                          'email': $('#email').val()})
+        send_data('signup', {'username': $('#username').val(),
+                             'password1': $('#password1').val(),
+                             'password2': $('#password2').val(),
+                             'email': $('#email').val()});
     });
 
     $('#redirect_to_login_btn').click(function(event){
